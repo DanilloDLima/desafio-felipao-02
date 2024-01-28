1c3f95a404579bc2509f5f5595dbe18e679092eb
DanilloDLima <danillodiasdlima@gmail.com>
2024-01-28 10:34:41 -0300

    Projeto Concluido - Adicionado função de classificação da ranked

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-let resultadoVitDer = saldoVitDer(20, 10)
+let resultadoVitDer = saldoVitDer(21, 0)
 
 classificacaoRanked(resultadoVitDer)
 
@@ -7,9 +7,19 @@ function saldoVitDer (vitorias, derrotas){
 }
 
 function classificacaoRanked(resultadoVitDer){
-    if(resultadoVitDer === 10){
-        return console.log("Funcionou")
+    if(resultadoVitDer <= 10){
+        return console.log(`O Herói tem de saldo de ${resultadoVitDer} vitórias, é está no nível FERRO`)
+    }else if(resultadoVitDer <= 20){
+        return console.log(`O Herói tem de saldo de ${resultadoVitDer} vitórias, é está no nível BRONZE`)
+    }else if(resultadoVitDer <= 50){
+        return console.log(`O Herói tem de saldo de ${resultadoVitDer} vitórias, é está no nível PRATA`)
+    }else if(resultadoVitDer <= 80){
+        return console.log(`O Herói tem de saldo de ${resultadoVitDer} vitórias, é está no nível OURO`)
+    }else if(resultadoVitDer <= 90){
+        return console.log(`O Herói tem de saldo de ${resultadoVitDer} vitórias, é está no nível DIAMANTE`)
+    }else if(resultadoVitDer <= 100){
+        return console.log(`O Herói tem de saldo de ${resultadoVitDer} vitórias, é está no nível LENDÁRIO`)
+    }else{
+        return console.log(`O Herói tem de saldo de ${resultadoVitDer} vitórias, é está no nível IMORTAL`)
     }
-
-    return console.log("Não funcionou")
 }
